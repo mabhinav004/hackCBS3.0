a0376bacaf8928691880ba158d3d21d57c86dc42
Ritwik <rtvksingh@gmail.com>
2020-11-07 14:35:37 +0530

    fetching contacts and displaying common Contacts

--- app/src/main/java/com/example/hackcbs30/MainFragment.java
+++ app/src/main/java/com/example/hackcbs30/MainFragment.java
@@ -1,23 +1,33 @@
 package com.example.hackcbs30;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
-
+    Button contacts;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.fragment_main,container,false);
         //initViews(view);
-
+        contacts=view.findViewById(R.id.GetContacts);
+        contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(),ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/hackcbs30/StartActivity.java
+++ app/src/main/java/com/example/hackcbs30/StartActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -26,6 +27,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
         initViews();
 
         setSupportActionBar(toolbar);
