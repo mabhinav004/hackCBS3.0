6e442b556a74b170f6597efd19ab9b93bde8b535
saketkumar1 <66872047+saketkumar1@users.noreply.github.com>
2020-11-07 15:32:20 +0530

    register

--- app/src/main/java/com/example/hackcbs30/StartActivity.java
+++ app/src/main/java/com/example/hackcbs30/StartActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -21,12 +22,22 @@ public class StartActivity extends AppCompatActivity {
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private MaterialToolbar toolbar;
+    Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         initViews();
+
+        reg=findViewById(R.id.register);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), register.class));
+            }
+        });
 
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
